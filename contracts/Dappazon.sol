68409a623769bca285b380839891e65cb0394526
Crys <mariano.christopher123@gmail.com>
2023-06-08 13:10:51 +0800

    updated contract

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,48 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // list products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public {
+        // create item struct
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+        // save item struct to blockchain
+        items[_id] = item;
+    }
+    // buy products
+    // withdraw funds
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,35 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("Dappazon", () => {
+  let dappazon;
+  let deployer, buyer;
+  beforeEach(async () => {
+    // setup accounts
+    [deployer, buyer] = await ethers.getSigners();
 
-})
+    // deploy contract
+    dappazon = await (await ethers.getContractFactory("Dappazon")).deploy();
+  });
+  describe("Deployment", () => {
+    it("Set the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    });
+  });
+  describe("Listing", () => {
+    let transaction;
+    beforeEach(async () => {
+      transaction = await dappazon
+        .connect(deployer)
+        .list(1, "Shoes", "Clothing", "IMAGE", 1, 4, 5);
+      await transaction.wait();
+    });
+    it("Returns item attributes", async () => {
+      const item = await dappazon.items(1);
+      expect(item.id).to.equal(1);
+    });
+  });
+});
