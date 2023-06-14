dbd0300e9bfc73caeaa1f7f527dff6d25e6a7746
Crys <mariano.christopher123@gmail.com>
2023-06-14 10:53:03 +0800

    added contract to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,28 @@ import Dappazon from "./abis/Dappazon.json";
 // Config
 import config from "./config.json";
 
+// address
+import config from "./config.json";
+
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [contract, setContract] = useState(null);
   const [account, setAccount] = useState(null);
+
   const loadBlockchainData = async () => {
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    const account = ethers.utils.getAddress(accounts[0]);
-    setAccount(account);
+    // connest to blockchain
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    // const address = config[network]["dappazon"][0];
+    const address = config[network.chainId].dappazon.address;
+    const abi = Dappazon;
+    // connect to smart contracts(Create JS Versions)
+    const dappazon = new ethers.Contract(address, abi, signerOrProvider);
+    setContract(dappazon);
+    // Load products
+    const items = [];
   };
 
   useEffect(() => {
@@ -29,7 +43,7 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-      <h2>Welcome to Dappazon</h2>
+      <h2>Dappazon Best Sellers</h2>
     </div>
   );
 }
